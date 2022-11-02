3b9353a47cde305399794bc0c450a110bb38c47a
Igor Renan <igorenansc@gmail.com>
2022-11-02 07:44:04 -0300

    requisito 6 | table header

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import './Table.css';
+
+const tableHeading = ['Descrição', 'Tag', 'Método de pagamento', 'Valor', 'Moeda',
+  'Câmbio utilizado', 'Valor convertido', 'Moeda de conversão', 'Editar/Excluir'];
 
 class Table extends Component {
   render() {
     return (
-      <div>Table</div>
+      <section className="expenses-table">
+        <table>
+          <thead>
+            <tr className="table-heading">
+              {tableHeading.map((h, i) => <th key={ i }>{h}</th>)}
+            </tr>
+          </thead>
+        </table>
+      </section>
     );
   }
 }
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import Header from '../components/Header';
+import Table from '../components/Table';
 import WalletForm from '../components/WalletForm';
 import './Wallet.css';
 
 class Wallet extends React.Component {
   render() {
     return (
-      <div className="form-header-container">
-        <Header />
-        <WalletForm />
-      </div>
+      <>
+        <div className="form-header-container">
+          <Header />
+          <WalletForm />
+        </div>
+        <div>
+          <Table />
+        </div>
+      </>
     );
   }
 }
